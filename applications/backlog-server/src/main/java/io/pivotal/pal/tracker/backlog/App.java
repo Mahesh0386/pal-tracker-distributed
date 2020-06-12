80a47ec27c97ee7f6a88f94e071371c69ad7a4d4
Mahesh <mahesh.bhannur@gmail.com>
2020-06-12 16:35:31 +0000

    Get solution and push

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -4,27 +4,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
 
-import javax.sql.DataSource;
-import java.util.Map;
 import java.util.TimeZone;
 
 
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.backlog", "io.pivotal.pal.tracker.restsupport"})
-@EnableEurekaClient
 public class App {
 
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-        ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
-        Map<String, DataSource> dataSourceMap = run.getBeansOfType(DataSource.class);
-        dataSourceMap.forEach((s, dataSource) -> System.out.println(dataSource.toString() + " ::"+ s));
-
+        SpringApplication.run(App.class, args);
     }
 
     @Bean
