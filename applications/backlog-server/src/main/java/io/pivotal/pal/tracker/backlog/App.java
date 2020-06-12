68275f6798cff6f55d578d350784f8d42b38007f
Mahesh <mahesh.bhannur@gmail.com>
2020-06-12 16:23:37 +0000

    Compare solution and update manifest.yml

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
 
+import javax.sql.DataSource;
+import java.util.Map;
 import java.util.TimeZone;
 
 
@@ -18,7 +21,10 @@ public class App {
 
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-        SpringApplication.run(App.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
+        Map<String, DataSource> dataSourceMap = run.getBeansOfType(DataSource.class);
+        dataSourceMap.forEach((s, dataSource) -> System.out.println(dataSource.toString() + " ::"+ s));
+
     }
 
     @Bean
